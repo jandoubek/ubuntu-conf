0038428c6837971be69a6533f9d5a81b71f7b0ea
jdoubek <mr.jan.doubek@gmail.com>
2017-02-01 15:51:17 +0100

    Fixing paths

--- repos.pp
+++ repos.pp
@@ -1,7 +1,7 @@
-vcsrepo { '/home/doubek/code/fei':
+vcsrepo { '/home/jdoubek/code/fei':
   ensure   => latest,
   provider => git,
   source   => 'gitlab:FEI/D2i.git',
-  user     => 'doubek', #uses doubek's $HOME/.ssh config
+  user     => 'jdoubek', #uses doubek's $HOME/.ssh config
   #require  => File['key'],
 }
--- ssh-config.pp
+++ ssh-config.pp
@@ -1,1 +1,5 @@
-
+file {'/home/jdoubek/.ssh/config':
+  path => '/home/jdoubek/.ssh/config',
+  ensure => present,
+  content => template('/home/jdoubek/ubuntu-conf/ssh-config.erb'),
+}
