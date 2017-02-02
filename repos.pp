2551e332790421a152eeaadd7d281b04c0ce74b5
jdoubek <mr.jan.doubek@gmail.com>
2017-02-02 22:50:53 +0100

    added personal repos

--- repos.pp
+++ repos.pp
@@ -5,3 +5,19 @@ vcsrepo { '/home/jdoubek/code/fei':
   user     => 'jdoubek', #uses doubek's $HOME/.ssh config
   #require  => File['key'],
 }
+
+vcsrepo { '/home/jdoubek/code/investment':
+  ensure   => latest,
+  provider => git,
+  source   => 'github:jandoubek/investment.git',
+  user     => 'jdoubek', #uses doubek's $HOME/.ssh config
+  #require  => File['key'],
+}
+
+vcsrepo { '/home/jdoubek/code/ubuntu-conf':
+  ensure   => latest,
+  provider => git,
+  source   => 'github:jandoubek/ubuntu-conf.git',
+  user     => 'jdoubek', #uses doubek's $HOME/.ssh config
+  #require  => File['key'],
+}
